3a069956c0380686d705a78096226dba5d43f1e6
Nestor Tamares <jrtamares@gmail.com>
2019-01-08 21:46:13 -0600

    Add embedded video, styling

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 const VideoDetail = ({video}) => {
   if(!video){
-    return <div>Loading...</div>
+    return <div>Search for a Video!</div>
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return(
-    <div className = "ui segment">
-      <h4 className = "ui header">
-        {video.snippet.title}
-      </h4>
-      <p>
-        {video.snippet.description}
-      </p>
+    <div>
+      <div className = "ui embed">
+        <iframe title = "video player" src = {videoSrc} />
+      </div>
+      <div className = "ui segment">
+        <h4 className = "ui header">
+          {video.snippet.title}
+        </h4>
+        <p>
+          {video.snippet.description}
+        </p>
+      </div>
     </div>
   );
 }
